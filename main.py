68d1a0aa8808054f944b54ffd59f63b1fb8f91b0
RobolutionClub <robolutionclub@gmail.com>
2022-06-03 18:24:18 +0530

    hola

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, redirect, url_for, flash,abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from sqlalchemy import Table, Column, Integer, ForeignKey
-
+import os
 from datetime import date
 from flask_wtf import FlaskForm
 from click import password_option
@@ -24,7 +24,7 @@ from sqlalchemy.orm import relationship
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
@@ -39,7 +39,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog-7.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog-7.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
